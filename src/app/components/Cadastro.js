d2bd99d1a9c8210e6ce41ff2fe2da85fd9fae875
klenilsonrox <klenilsoncoc@gmail.com>
2024-06-30 18:51:40 -0300

    att

--- src/app/components/Cadastro.js
+++ src/app/components/Cadastro.js
@@ -96,12 +96,12 @@ const Cadastro = () => {
             <input type="email" placeholder='Seu email' value={email} onChange={({target})=>setEmail(target.value)} className='bg-gray-200 p-3 rounded-md outline-none'/>
         </div>
         <div className='flex flex-col mt-4'>
-            <label htmlFor="password">Confirme sua senha</label>
-            <input type="password" placeholder='********' value={password} onChange={({target})=>setPassword(target.value)} className={ `bg-gray-200 p-3 rounded-md outline-none  `}/>
-        </div>
-        <div className='flex flex-col mt-4'>
             <label htmlFor="senha">Senha</label>
             <input type="password" placeholder='********' value={confirmPass} onChange={({target})=>setConfirmPassword(target.value)} className={ `bg-gray-200 p-3 rounded-md outline-none  `}/>
+        </div>
+        <div className='flex flex-col mt-4'>
+            <label htmlFor="password">Confirme sua senha</label>
+            <input type="password" placeholder='********' value={password} onChange={({target})=>setPassword(target.value)} className={ `bg-gray-200 p-3 rounded-md outline-none  `}/>
         </div>
         <button className='mt-6 bg-red-600 rounded-md text-white py-3 mb-6 flex items-center justify-center' disabled={loading}> {loading && <div className='w-6 h-6 rounded-full border-4 border-r-transparent animate-spin'></div> } {!loading && <span>Cadastrar</span>}</button>
         <div className='flex gap-4'>
